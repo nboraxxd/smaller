c302e631544afd4e5d30ea6a7f862f0c7c48c6d2
bora <nhatbaoxxd@gmail.com>
2024-06-18 18:39:41 +0700

    validate env variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function Homepage() {
-  return <div></div>
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
 }
